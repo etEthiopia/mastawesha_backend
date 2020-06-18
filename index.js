b752f01687d6c9d2ad867f9333ba5a2bca4e9312
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-18 20:40:45 +0300

    backend

---
+++ index.js
@@ -0,0 +1,36 @@
+// dotenv
+require("dotenv").config();
+
+//required
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//routes
+
+
+// instantiate Express
+const app = express();
+
+
+// use the imports
+app.use(bodyParser.json());
+app.use(cors());
+
+// use routes
+
+
+mongoose
+    .connect(process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to the Database"))
+    .catch(err => console.log("DB ERROR : " + err));
+
+const port = process.env.PORT || 5000;
+
+// Server
+app.listen(port, () => console.log("Server has Started"));
