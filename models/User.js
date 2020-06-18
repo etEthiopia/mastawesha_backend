42e4683f43df2756e94ca7b1770cf31b0416de39
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-18 21:52:08 +0300

    first trial users

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports.toJSON = function (obj) {
+    var obj = this.toObject()
+    delete obj.password
+    return obj
+}
+
+module.exports = mongoose.model("User", UserSchema);
